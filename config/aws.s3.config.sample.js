e06abfb3fde71df9eb615b0a90f23cc47512a052
nastya <lebedeanastasija@yandex.by>
2018-01-20 13:59:14 +0300

    Fix db models

---
+++ config/aws.s3.config.sample.js
@@ -0,0 +1,7 @@
+module.exports.s3Config = {
+    accessKeyId: "access key id",
+    secretAccessKey: "secret access key"
+};
+
+module.exports.s3Region = 'region';
+module.exports.s3Bucket = 'bucket name';
